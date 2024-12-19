123dfe954667a02119f32660da4430cd3f491854
hayashun <154653391+hayashunkus@users.noreply.github.com>
2024-12-19 16:27:12 +0900

    カードクリックでHP減少

--- Assets/Prefabs/Card/HP.cs
+++ Assets/Prefabs/Card/HP.cs
@@ -6,23 +6,40 @@ using UnityEngine.UI;
 public class HP : MonoBehaviour
 {
     //最大HPと現在のHP。
-    int maxHp = 155;
-    int currentHp;
+   　public int maxHp = 155;
+     private int currentHp;
     //Sliderを入れる
-    public Slider slider;
+    public Slider opponentHpSlider;
 
     // Start is called before the first frame update
     void Start()
     {
         //Sliderを満タンにする。
-        slider.value = 1;
+        opponentHpSlider.value = 1;
         //現在のHPを最大HPと同じに。
         currentHp = maxHp;
         Debug.Log("Start currentHp : " + currentHp);
     }
 
-    // Update is called once per frame
-    void Update()
+    // HPを減少させるメソッド
+    public void ReduceHp(int damage)
+    {
+        // ダメージ分HPを減少
+        currentHp -= damage;
+
+        // HPが0未満にならないように制御
+        if (currentHp < 0)
+        {
+            currentHp = 0;
+        }
+
+        // Sliderの値を更新
+        opponentHpSlider.value = currentHp;
+
+        Debug.Log("Current HP after damage: " + currentHp);
+    }
+// Update is called once per frame
+void Update()
     {
         
     }
---
+++ Assets/Prefabs/Card/damage.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class damage : MonoBehaviour
+{
+    public Slider opponentHpSlider; // 相手のHPゲージ（Slider）
+    public int attackDamage = 10;  // カードの攻撃力
+
+    // カードがクリックされたときに呼び出される
+    public void OnCardClicked()
+    {
+        if (opponentHpSlider != null)
+        {
+            // 現在のHPを減少
+            opponentHpSlider.value -= attackDamage;
+
+            // HPが0未満にならないように制御
+            if (opponentHpSlider.value <= 0)
+            {
+                opponentHpSlider.value = 0;
+                Debug.Log("相手のHPが0になりました！");
+            }
+        }
+        else
+        {
+            Debug.LogError("Opponent HP Slider is not assigned!");
+        }
+    }
+        // Start is called before the first frame update
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
