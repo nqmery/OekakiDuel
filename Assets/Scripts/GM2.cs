d98379b85ee6c4d5a7f084c6e9f7f5733c22625b
hayashun <154653391+hayashunkus@users.noreply.github.com>
2024-12-19 23:39:09 +0900

    attackボタンでHP減少

---
+++ Assets/Scripts/GM2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GM2 : MonoBehaviour
+{
+    Slider HPbar;
+    public int HP =150;
+    public bool Attack_flag=false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HPbar=GameObject.Find("HPbar").GetComponent<Slider>();
+        HPbar.maxValue = HP;
+        HPbar.value = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Attack_flag == true)
+        {
+            HPbar.value -= 10;
+        }
+    }
+    public void Attack()
+    {
+        Attack_flag = true;
+    }
+}
