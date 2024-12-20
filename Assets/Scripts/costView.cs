4d932dfde04a53dfc7f49c6a1d4732d1e2d3719c
Name <68557173+nqmery@users.noreply.github.com>
2024-12-20 10:57:14 +0900

    costView

---
+++ Assets/Scripts/costView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class costView : MonoBehaviour
+{
+    Text textComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.textComponent = this.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.textComponent.text = "Cost: " + BattleManager.battleManager.cost;
+    }
+}
